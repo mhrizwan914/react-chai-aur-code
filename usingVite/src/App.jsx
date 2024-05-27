cd43368f6a20addd510663f57ea15b35cc322ca3
Muhammad Rizwan <mhrizwan914@gmail.com>
2024-05-27 16:23:56 +0500

    Learn Components & File Structure

--- usingVite/src/App.jsx
+++ usingVite/src/App.jsx
@@ -1,5 +1,12 @@
+import Chai from "./Chai"
+
 function App() {
-  return <h1>Hello Rizwan - Vite</h1>
+  return (
+    <>
+      <h1>Hello Rizwan - Vite</h1>
+      <Chai />
+    </>
+  )
 }
 
 export default App
